d6d547ac453916f7768658215d547077abe8937d
tqacyp <tqacyp@163.com>
2025-08-02 21:28:37 +0800

    适用于ngrok+修改消息内容

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def handle_send_message(data):
     emit('new_message', new_message, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='127.0.0.1', port=80, debug=False)
